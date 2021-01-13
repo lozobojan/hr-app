a70bf8290a1f50f53f847f528e4151a74120e5c2
Radule-Bulatovic <66704768+Radule-Bulatovic@users.noreply.github.com>
2021-01-13 14:48:04 +0100

    Fix

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,53 +1,29 @@
-// function navItem(data, route, target){
-//     if(data.length == 0){
-//         $(`#target-${target}`).append(`<li class="nav-item"><a href="#" class="nav-link"><p>Nema podataka</p></a></li>`);
-//     }
-//     else{
-//         for(var i = 0; i < data.length; i++){
-//             $(`#target-${target}`).append(`<li class="nav-item"><a href="/${response.data.files[i].file_path}" target="_blank" class="nav-link"><i class="far fa-circle nav-icon"></i><p>${response.data.files[i].name}</p></a></li>`);
-//         }
-//     }
-// }
+function navItem(data, currentURL){
+    var keyValues = Object.keys(data);
+    var url = ["/directory/","/","/tag/type/","/tag/sector/"];
+    for(var j = 0; j < keyValues.length; j++){
+        if(data[keyValues[j]].length == 0){
+            $(`#target-${keyValues[j]}`).append(`<li class="nav-item"><a href="#" class="nav-link"><p>Nema podataka</p></a></li>`);
+        }
+        else{
+            var deepKeys = Object.keys(data[keyValues[j]][0]);
+            for(var i = 0; i < data[keyValues[j]].length; i++){
+                $(`#target-${keyValues[j]}`).append(`<li class="nav-item"><a href="${url[j]+data[keyValues[j]][i][deepKeys[0]]}" ${j == 1 ? 'target="_blank"' : ""} class="nav-link ${ currentURL.includes(url[j]+data[keyValues[j]][i][deepKeys[0]]) ? "active" : ""}"><i class="far fa-circle nav-icon"></i><p>${data[keyValues[j]][i][deepKeys[1]]}</p></a></li>`);
+            }
+        }
+    }
+    return;
+}
 
 $(document).ready(function() {
     $('.preloader').hide();
 
     axios.get('/api/directories')
         .then((response) => {
-            url = `{{url()->current()}}`;
-            if(response.data.directories.length == 0){
-                $("#target-dir").append(`<li class="nav-item"><a href="#" class="nav-link"><p>Nema foldera</p></a></li>`);
-            }
-            for(var i = 0; i < response.data.directories.length; i++){
-                if(url.substring(22) == 'directory/'+response.data.directories[i].id){
-                    $("#target-dir").append(`<li class="nav-item"><a href="/directory/${response.data.directories[i].id}" class="nav-link active"><i class="far fa-circle nav-icon"></i><p>${response.data.directories[i].name}</p></a></li>`);
-                    continue;
-                }
-                $("#target-dir").append(`<li class="nav-item"><a href="/directory/${response.data.directories[i].id}" class="nav-link"><i class="far fa-circle nav-icon"></i><p>${response.data.directories[i].name}</p></a></li>`);
-            }
-            if(response.data.files.length == 0){
-                $("#target-file").append(`<li class="nav-item"><a href="#" class="nav-link"><p>Nema dokumenata</p></a></li>`);
-            }
-            for(var i = 0; i < response.data.files.length; i++){
-                $("#target-file").append(`<li class="nav-item"><a href="/${response.data.files[i].file_path}" target="_blank" class="nav-link"><i class="far fa-circle nav-icon"></i><p>${response.data.files[i].name}</p></a></li>`);
-            }
-            if(response.data.sectors.length == 0){
-                $("#target-sector").append(`<li class="nav-item"><a href="#" class="nav-link"><p>Nema sektora</p></a></li>`);
-            }
-            for(var i = 0; i < response.data.sectors.length; i++){
-                if(url.substring(22) == 'tag/sector/'+response.data.sectors[i].id){
-                    $("#target-sector").append(`<li class="nav-item"><a href="/tag/sector/${response.data.sectors[i].id}" class="nav-link active"><i class="far fa-circle nav-icon"></i><p>${response.data.sectors[i].name}</p></a></li>`);
-                    continue;
-                }
-                $("#target-sector").append(`<li class="nav-item"><a href="/tag/sector/${response.data.sectors[i].id}" class="nav-link"><i class="far fa-circle nav-icon"></i><p>${response.data.sectors[i].name}</p></a></li>`);
-            }
-            for(var i = 0; i < response.data.types.length; i++){
-                if(url.substring(22) == 'tag/type/'+response.data.sectors[i].id){
-                    $("#target-type").append(`<li class="nav-item"><a href="/tag/type/${response.data.types[i].id}" class="nav-link active"><i class="far fa-circle nav-icon"></i><p>${response.data.types[i].name}</p></a></li>`);
-                    continue;
-                }
-                $("#target-type").append(`<li class="nav-item"><a href="/tag/type/${response.data.types[i].id}" class="nav-link"><i class="far fa-circle nav-icon"></i><p>${response.data.types[i].name}</p></a></li>`);
-            }
+            var test = Object.keys(response.data);
+            url = window.location.href;
+            navItem(response.data, url);
+
         });
     $("#search").click(function(){
         var keyword = $("#keyword").val();
